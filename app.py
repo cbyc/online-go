71f4fe34870ac3b69985b3bf97e601e772e1e722
Mehdi Khajeh <mehdi.khajeh@thoughtworks.com>
2023-05-14 21:39:48 +0200

    add undo functionality

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ from collections import defaultdict
 import os
 import random
 import string
+import copy
 
 app = Flask(__name__, static_folder='static')
 
-games = defaultdict(lambda: [[None for _ in range(9)] for _ in range(9)])
+games = defaultdict(
+    lambda: {
+        "board": [[None for _ in range(9)] for _ in range(9)],
+        "current_player": "black",
+        "history": []
+    })
 
 
 @app.route('/games/<game_id>', methods=['GET', 'POST'])
@@ -17,6 +23,7 @@ def game(game_id):
         x, y, color = data['x'], data['y'], data['color']
         if game["board"][x][y] is not None:
             return 'This cell is already occupied!', 400  # Bad Request
+        game["history"].append(copy.deepcopy(game["board"]))
         game["board"][x][y] = color
         game[
             "current_player"] = "white" if color == "black" else "black"  # Switch the current player
@@ -27,14 +34,32 @@ def game(game_id):
 
 @app.route('/games/<game_id>/state', methods=['GET'])
 def game_state(game_id):
-    return jsonify(games[game_id])
+    game = games[game_id]
+    return jsonify({
+        "board": game["board"],
+        "current_player": game["current_player"]
+    }), 200
+
+
+@app.route('/games/<game_id>/undo', methods=['POST'])
+def undo(game_id):
+    game = games[game_id]
+    if game["history"]:
+        game["board"] = game["history"].pop()  # Revert to the last state
+        game["current_player"] = "white" if game[
+            "current_player"] == "black" else "black"  # Switch the current player
+    return {
+        "board": game["board"],
+        "current_player": game["current_player"]
+    }, 200
 
 
 @app.route('/games/<game_id>/reset', methods=['POST'])
 def reset_game(game_id):
     games[game_id] = {
         "board": [[None for _ in range(9)] for _ in range(9)],
-        "current_player": "black"
+        "current_player": "black",
+        "history": []
     }
     return '', 204  # No Content
 
@@ -48,7 +73,8 @@ def new_game():
             random.choices(string.ascii_letters + string.digits, k=7))
     games[game_id] = {
         "board": [[None for _ in range(9)] for _ in range(9)],
-        "current_player": "black"
+        "current_player": "black",
+        "history": []
     }
     return {'game_id': game_id}, 201  # Created
 
